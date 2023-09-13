4d944f4646e604f94cf6506e84594f66b129541a
Gustavo Ribeiro Santiago <142454616+gustavo-ribeiro-santiago@users.noreply.github.com>
2023-09-13 17:06:45 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,366 @@
+var pinturas = [
+  {
+    nome: "Impression, sunrise",
+    autor: "Claude Monet",
+    ano: 1872,
+    arquivo: "./impression-sunrise.jpg",
+    descricao: `
+    When Impression, Sunrise hung at its first exhibition in 1874, art critic Louis Leroy derisively used the term “Impressionistic,” from the title of this painting, to describe Monet’s works. 
+    This term was quickly adopted by what were soon to be known as the Impressionist painters, and the exhibition which included other works by Impressionist artists, was from then on referred to as the “Impressionist Exhibition.” 
+    This painting was later stolen in 1985 from the Musee Marmottan Monet in Paris, but was recovered undamaged in 1990, and was put back on display at the museum in 1991.
+    `,
+    wikipediaLink: "https://en.wikipedia.org/wiki/Impression,_Sunrise",
+  },
+  {
+    nome: "Guernica",
+    autor: "Pablo Picasso",
+    ano: 1937,
+    arquivo: "./guernica-by-pablo-picasso.jpg",
+    descricao: `
+    Guernica was Picasso's response to the bombing of the Basque town of the same name on April 26, 1937 during the Spanish Civil War. Picasso was commissioned by the republican government of Spain to produce a mural painting for the Spanish Pavilion at the World Fair in Paris. Painted in one month - from May to June 1937 - Guernica became the centerpiece of the Spanish pavilion and a sensation at the Fair, but it was consequently banned from exhibition in Spain until military dictator Franco fell from power in 1975.
+    Picasso had studied dramatic photographs of bombing published in various periodicals. Despite that, neither the studies nor the finished picture contain a single allusion to a specific event, constituting instead a generic plea against the barbarity and terror of war. The scene depicted in Guernica is a room full of moving, screaming and dying adults, children and animals.
+    `,
+    wikipediaLink: "https://en.wikipedia.org/wiki/Nighthawks",
+  },
+  {
+    nome: "Nighthawks",
+    autor: "Edward Hopper",
+    ano: 1942,
+    arquivo: "./nighthawks.jpg",
+    descricao: `
+    Edward Hopper said that Nighthawks was inspired by "a restaurant on New York's Greenwich Avenue where two streets meet," but the image, with its carefully constructed composition and lack of narrative, has a timeless quality that transcends its particular locale. One of the best-known images of 20th-century art, the painting depicts an all-night diner in which three customers, all lost in their own thoughts, have congregated. Fluorescent lights had just come into use in the early 1940s, and the all-night diner emits an eerie glow, like a beacon on the dark street corner. Hopper eliminated any reference to an entrance, and the viewer, drawn to the light, is shut out from the scene by a seamless wedge of glass. The four anonymous and uncommunicative night owls seem as separate and remote from the viewer as they are from one another. Reworked and parodied countless times, Nighthawks has become an icon of American culture.
+    `,
+    wikipediaLink: "https://en.wikipedia.org/wiki/Nighthawks",
+  },
+  {
+    nome: "The Great Wave off Kanagawa",
+    autor: "Katsushika Hokusai",
+    ano: 1831,
+    arquivo: "./the-great-wave-off-kanagawa.jpg",
+    descricao: `
+    The Great Wave off Kanagawa (神奈川沖浪裏, Kanagawa-oki nami ura, "Under a wave off Kanagawa"), also known as The Great Wave or simply The Wave, is a woodblock print by the Japanese ukiyo-e artist Hokusai. It was published sometime between 1829 and 1833 in the late Edo period as the first print in Hokusai's series Thirty-six Views of Mount Fuji. It is Hokusai's most famous work, and one of the most recognizable works of Japanese art in the world.
+    The image depicts an enormous wave threatening boats off the coast of the town of Kanagawa (the present-day city of Yokohama, Kanagawa Prefecture). While sometimes assumed to be a tsunami, the wave is more likely to be a large rogue wave. As in many of the prints in the series, it depicts the area around Mount Fuji under particular conditions, and the mountain itself appears in the background.
+    `,
+    wikipediaLink: "https://en.wikipedia.org/wiki/The_Great_Wave_off_Kanagawa",
+  },
+  {
+    nome: "The Kiss",
+    autor: "Gustav Klimt",
+    ano: 1908,
+    arquivo: "./the-kiss-1908.jpg",
+    descricao: `
+    The Kiss is probably Gustav Klimt’s most famous work. It is also the high point of the artist’s Gold Period, which was characterized by his use of gold leaf in his work. This painting is one in which Klimt deviated from his portrayal of dominant in women in the form of a femme fatale. Instead is the portrayal of love and art, a couple locked in a golden-flecked, flower-filled embrace. Klimt was a man with an unbridled sexual appetite, as he fathered at least 14 illegitimate children. It is rumored that Klimt and his longtime companion, Emile Floge, who was also said to be his lover, were the models of the painting, which was selected to be printed on the Austrian 100 euro coin, minted in 2003.
+    `,
+    wikipediaLink: "https://en.wikipedia.org/wiki/The_Kiss_(Klimt)",
+  },
+  {
+    nome: "Self Portrait - Time Flies",
+    autor: "Frida Kahlo",
+    ano: 1929,
+    arquivo: "./self-portrait-time-flies-1929.jpg",
+    descricao: `
+    Frida Kahlo de Rivera (Spanish pronunciation: [ˈfɾiða ˈkalo]; born Magdalena Carmen Frida Kahlo y Calderón; July 6, 1907 – July 13, 1954) was a Mexican artist who painted many portraits, self-portraits, and works inspired by the nature and artifacts of Mexico. Inspired by the country's popular culture, she employed a naïve folk art style to explore questions of identity, postcolonialism, gender, class, and race in Mexican society.
+    `,
+    wikipediaLink: "https://en.wikipedia.org/wiki/Impression,_Sunrise",
+  },
+  {
+    nome: "The Last Supper",
+    autor: "Leonardo da Vinci",
+    ano: 1495,
+    arquivo: "./the-last-supper-restored-da-vinci.jpg",
+    descricao: `
+    The Last Supper (Italian: Il Cenacolo [il tʃeˈnaːkolo] or L'Ultima Cena [ˈlultima ˈtʃeːna]) is a late 15th-century mural painting by Italian artist Leonardo da Vinci housed by the refectory of the Convent of Santa Maria delle Grazie in Milan, Italy. It is one of the world's most recognizable paintings.
+    The work is presumed to have been started around 1495–96 and was commissioned as part of a plan of renovations to the church and its convent buildings by Leonardo's patron Ludovico Sforza, Duke of Milan. The painting represents the scene of the Last Supper of Jesus with his apostles, as it is told in the Gospel of John, 13:21. Leonardo has depicted the consternation that occurred among the Twelve Disciples when Jesus announced that one of them would betray him.
+    `,
+    wikipediaLink:
+      "https://en.wikipedia.org/wiki/The_Last_Supper_(Leonardo_da_Vinci)",
+  },
+  {
+    nome: "The Persistence of Memory",
+    autor: "Salvador Dali",
+    ano: 1931,
+    arquivo: "./the-persistence-of-memory-1931.jpg",
+    descricao: `
+    The Persistence of Memory (Spanish: La persistencia de la memoria) is a 1931 painting by artist Salvador Dalí, and one of his most recognizable works. First shown at the Julien Levy Gallery in 1932, since 1934 the painting has been in the collection of the Museum of Modern Art (MoMA) in New York City, which received it from an anonymous donor. It is widely recognized and frequently referenced in popular culture, and sometimes referred to by more descriptive (though incorrect) titles, such as "Melting Clocks", "The Soft Watches" or "The Melting Watches".
+    `,
+    wikipediaLink: "https://en.wikipedia.org/wiki/The_Persistence_of_Memory",
+  },
+  {
+    nome: "The Scream",
+    autor: "Edvard Munch",
+    ano: 1893,
+    arquivo: "./the-scream-1893.jpg",
+    descricao: `
+    The Scream is the best known and most frequently reproduced of all Munch’s motifs. With its expressive colours, its flowing lines and striking overall effect, its appeal is universal.
+    Despite radical simplification, the landscape in the picture is recognisable as the Kristiania Fjord seen from Ekeberg, with a broad view over the fjord, the town and the hills beyond. In the background to the left, at the end of the path with the balustrade that cuts diagonally across the picture, we see two strolling figures, often regarded as two friends whom Munch mentions in notes relating to the picture. But the figure in the foreground is the first to capture the viewer’s attention. Its hands are held to its head and its mouth is wide open in a silent scream, which is amplified by the undulating movement running through the surrounding landscape.
+    `,
+    wikipediaLink: "https://en.wikipedia.org/wiki/The_Scream",
+  },
+  {
+    nome: "The Starry Night",
+    autor: "Vincent van Gogh",
+    ano: 1889,
+    arquivo: "./the-starry-night.jpg",
+    descricao: `
+    The Starry Night is an oil on canvas by the Dutch post-impressionist painter Vincent van Gogh. Painted in June 1889, it depicts the view from the east-facing window of his asylum room at Saint-Rémy-de-Provence, just before sunrise, with the addition of an idealized village.
+    The Starry Night is regarded as among Van Gogh's finest works, and is one of the most recognized paintings in the history of Western culture. It has been in the permanent collection of the Museum of Modern Art in New York City since 1941, acquired through the Lillie P. Bliss Bequest.
+    `,
+    wikipediaLink: "https://en.wikipedia.org/wiki/The_Starry_Night",
+  },
+];
+
+var circlesIdsArray = []; //a array para os ids dos círculos criados
+var qtdCirculosTotal = 0; //quantidade total de círculos utilizados para encobrir a imagem
+var qtdCliquesAteAgora = 0; //quantidade de cliques já realizados para a mostrar a pintura nesta fase
+var qtdCliquesAtePinturaCompleta = 4; //quantidade de cliques até a pintura ser totalmente exibida
+var renderizacaoPorContaDeMudancaDeTela = false; //motivo de renderização
+var pinturasRodadasPassadas = []; //array para guardar pinturas que já apareceram em rodadas passadas
+
+function adicionarOpcoesDeResposta(pintura) {
+  let opcoesDeResposta = criarListaDeOpcoesResposta();
+  for (let numeroOpcao = 0; numeroOpcao < 5; numeroOpcao++) {
+    opcoesElementoHTML = document.createElement("input");
+    opcoesElementoHTML.type = "radio";
+    opcoesElementoHTML.id = `opcaoRadio${numeroOpcao}`;
+    opcoesRotuloElementoHTML = document.createElement("label");
+    opcoesRotuloElementoHTML.for = "opcaoRadio";
+    let pinturaOpcao = opcoesDeResposta.sample();
+    opcoesDeResposta.splice(opcoesDeResposta.indexOf(pinturaOpcao), 1);
+    opcoesRotuloElementoHTML.innerText =
+      String.fromCharCode("a".charCodeAt(0) + numeroOpcao) +
+      `) ${pinturaOpcao.nome} by ${pinturaOpcao.autor}`;
+    if (numeroOpcao < 4) {
+      opcoesRotuloElementoHTML.innerText += ",";
+    }
+    document.getElementById("opcoesResposta").appendChild(opcoesElementoHTML);
+    document
+      .getElementById("opcoesResposta")
+      .appendChild(opcoesRotuloElementoHTML);
+    opcoesRotuloElementoHTML.addEventListener("click", () =>
+      conferirResposta(pinturaOpcao)
+    );
+  }
+}
+
+function criarListaDeOpcoesResposta() {
+  let listaDeOpcoes = [pinturaRodadaAtual];
+  for (let i = 0; i < 4; ) {
+    pinturaAleatoria = pinturas.sample();
+    if (listaDeOpcoes.indexOf(pinturaAleatoria) === -1) {
+      listaDeOpcoes[listaDeOpcoes.length] = pinturaAleatoria;
+      i++;
+    }
+  }
+  return listaDeOpcoes;
+}
+
+function adicionarPintura(pintura) {
+  // Esta função adiciona a pintura ao site
+  var pinturaElementoHTML = document.createElement("img");
+  pinturaElementoHTML.src = `./assets/${pintura.arquivo}`;
+  pinturaElementoHTML.id = "pinturaImagem";
+  document.getElementById("pinturaConteiner").appendChild(pinturaElementoHTML);
+  //pinturaElementoHTML.style.width = "70%";
+  pinturaElementoHTML.onload = function (event) {
+    adicionarCirculosBranco(pinturaElementoHTML);
+  };
+  qtdCliquesAteAgora = 0;
+  return pinturaElementoHTML;
+}
+
+function adicionarCirculosBranco(pinturaElementoHTML) {
+  //Esta função remove os círculos brancos sobrepostos existentes e adiciona novos para esconder a pintura
+  const circles = document.getElementsByClassName("circles");
+  if (circles.length > 0) {
+    for (let i = circles.length - 1; i >= 0; i--) {
+      circles[i].remove();
+    }
+    circlesIdsArray = [];
+  }
+  //cria os círculos novos:
+  let larguraPintura = 0.7 * window.innerWidth;
+  let alturaPintura =
+    larguraPintura * (pinturaElementoHTML.height / pinturaElementoHTML.width);
+  let qtdCirculosHorizontal = larguraPintura / 30;
+  let qtdCirculosVertical = alturaPintura / 30;
+  qtdCirculosTotal =
+    Math.ceil(qtdCirculosHorizontal) * Math.ceil(qtdCirculosVertical);
+  let topPintura = document
+    .getElementById("pinturaConteiner")
+    .getBoundingClientRect().top;
+  for (var i = 0; i < qtdCirculosVertical; i++) {
+    for (var j = 0; j < qtdCirculosHorizontal; j++) {
+      //cria cada círculo:
+      circlesIdsArray.push(`c${i}/${j}`);
+      let circle = document.createElement("div");
+      circle.style.top = i * 30 - 10 + "px";
+      circle.style.left = j * 30 - 10 + "px";
+      circle.classList.add("circles");
+      circle.id = `c${i}/${j}`;
+      document.getElementById("pinturaConteiner").appendChild(circle);
+      console.log("test");
+    }
+  }
+  console.log(document.getElementsByClassName("circles").length);
+  mostrarPintura();
+}
+
+Array.prototype.sample = function () {
+  return this[Math.floor(Math.random() * this.length)];
+};
+
+function mostrarPintura() {
+  //Esta função mostra a pintura pouco a pouco escondendo os círculos brancos
+  qtdCliquesAteAgora++;
+  console.log(qtdCliquesAteAgora);
+  if (qtdCliquesAteAgora > qtdCliquesAtePinturaCompleta) {
+    return;
+  } else if (qtdCliquesAteAgora === qtdCliquesAtePinturaCompleta) {
+    mostrarPinturaCompleta();
+  } else {
+    //esconder mais círculos brancos:
+    if (renderizacaoPorContaDeMudancaDeTela === true) {
+      //esconder os circulos para resgatar rodada atual
+      qtdIteracoes =
+        (qtdCirculosTotal / qtdCliquesAtePinturaCompleta) * qtdCliquesAteAgora;
+    } else {
+      //esconder apenas mais uma rodada de círculos
+      qtdIteracoes = qtdCirculosTotal / qtdCliquesAtePinturaCompleta;
+    }
+    for (var i = 0; i < qtdIteracoes; i++) {
+      //esconde cada círculo branco:
+      let id = circlesIdsArray.sample();
+      circlesIdsArray.splice(circlesIdsArray.indexOf(id), 1);
+      const circle = document.getElementById(id);
+      circle.classList.add("hidden");
+    }
+  }
+}
+
+function mostrarPinturaCompleta() {
+  //esconder todos os círculos brancos restantes:
+  let qtdCirculosRestantes = circlesIdsArray.length;
+  for (var i = 0; i < qtdCirculosRestantes; i++) {
+    //esconde cada círculo branco:
+    let id = circlesIdsArray.sample();
+    circlesIdsArray.splice(circlesIdsArray.indexOf(id), 1);
+    const circle = document.getElementById(id);
+    circle.classList.add("hidden");
+  }
+}
+
+function adicionarBotaoMostrar() {
+  //Esta função adiciona o botão de mostrar a imagem
+  document.getElementById(
+    "botaoShow"
+  ).innerHTML += `<button id="botoes" class="botoes" onclick="mostrarPintura()"> Show more of the painting </button>`;
+}
+
+function renderizarNovamente(pinturaElementoHTML) {
+  qtdCliquesAteAgora--;
+  renderizacaoPorContaDeMudancaDeTela = true;
+  adicionarCirculosBranco(pinturaElementoHTML);
+}
+
+function conferirResposta(pintura) {
+  if (pintura.nome === pinturaRodadaAtual.nome) {
+    mostrarResolucao();
+  } else {
+    mostrarResolucao(false);
+  }
+}
+
+function mostrarResolucao(acerto = true) {
+  if (acerto) {
+    //Mostrar: 'Correct Answer!' in green
+    let tituloRespostaCorreta = document.createElement("h2");
+    tituloRespostaCorreta.id = "tituloRespostaCorreta";
+    document.getElementById("resolucao").appendChild(tituloRespostaCorreta);
+    tituloRespostaCorreta.innerText = `Correct Answer!`;
+  } else {
+    //Mostrar: 'Wrong Answer! The correct answer is:'. ('Wrong Answer' in red):
+    let conteinerRespostaErrada = document.createElement("div");
+    conteinerRespostaErrada.id = "conteinerRespostaErrada";
+    document.getElementById("resolucao").appendChild(conteinerRespostaErrada);
+    let tituloRespostaErrada = document.createElement("h2");
+    tituloRespostaErrada.id = "tituloRespostaErrada";
+    document
+      .getElementById("conteinerRespostaErrada")
+      .appendChild(tituloRespostaErrada);
+    tituloRespostaErrada.innerText = `Wrong Answer!`;
+    let respostaCorretaE = document.createElement("h2");
+    respostaCorretaE.innerText = `The correct answer is:`;
+    document
+      .getElementById("conteinerRespostaErrada")
+      .appendChild(respostaCorretaE);
+  }
+  //Mostrar nome e história da pintura:
+  let tituloResolucaoHTML = document.createElement("h2");
+  tituloResolucaoHTML.innerText = `"${pinturaRodadaAtual.nome}"
+  by ${pinturaRodadaAtual.autor}`;
+  document.getElementById("resolucao").appendChild(tituloResolucaoHTML);
+  let conteudoResolucaoHTML = document.createElement("div");
+  document.getElementById("resolucao").appendChild(conteudoResolucaoHTML);
+  conteudoResolucaoHTML.innerText += `
+  Painted in ${pinturaRodadaAtual.ano}. 
+  ${pinturaRodadaAtual.descricao} 
+  For more information, visit the Wikipedia article at: 
+  ${pinturaRodadaAtual.wikipediaLink}`;
+  qtdCliquesAteAgora = qtdCliquesAtePinturaCompleta;
+  mostrarPinturaCompleta();
+  adicionarBotaoProximaRodada();
+}
+
+function adicionarBotaoProximaRodada() {
+  let botaoProximaRodada = document.createElement("button");
+  document.getElementById("resolucao").appendChild(botaoProximaRodada);
+  botaoProximaRodada.innerText = "Next round →";
+  botaoProximaRodada.id = "botaoProximaRodada";
+  botaoProximaRodada.classList.add("botoes");
+  botaoProximaRodada.addEventListener("click", iniciarRodada);
+}
+
+function iniciarRodada() {
+  if (conferirSeJaForamTodasAsPinturas() === true) {
+    terminarJogo();
+  }
+  //selecionar pintura atual:
+  for (let i = 0; i < 1; ) {
+    let pinturaNovaRodada = pinturas.sample();
+    if (pinturasRodadasPassadas.indexOf(pinturaNovaRodada) === -1) {
+      pinturaRodadaAtual = pinturaNovaRodada;
+      i++;
+    }
+  }
+  pinturasRodadasPassadas.push(pinturaRodadaAtual);
+  //resetar tela:
+  if (document.body.contains(document.getElementById("pinturaImagem"))) {
+    document.getElementById("pinturaImagem").remove();
+  }
+  document.getElementById("resolucao").innerText = "";
+  document.getElementById("opcoesResposta").innerText = "";
+  //adicionar itens da nova rodada:
+  pinturaElementoHTML = adicionarPintura(pinturaRodadaAtual);
+  adicionarOpcoesDeResposta(pinturaRodadaAtual);
+}
+
+function conferirSeJaForamTodasAsPinturas() {
+  if (pinturasRodadasPassadas.length === pinturas.length) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function terminarJogo() {
+  let popUp = document.createElement("div");
+  popUp.id = "popUpFimDoJogo";
+  document.getElementById('corpoSite').appendChild(popUp);
+  popUp.innerText = "Congratulations! You finished the game!"
+}
+
+iniciarRodada();
+adicionarBotaoMostrar();
+window.addEventListener("resize", () =>
+  renderizarNovamente(pinturaElementoHTML)
+); //em caso de mudança do tamanho da tela
