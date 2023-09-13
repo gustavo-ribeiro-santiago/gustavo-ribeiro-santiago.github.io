5a407fe374bfa5390aa2d2f9af34622ca9978ef8
Gustavo Ribeiro Santiago <142454616+gustavo-ribeiro-santiago@users.noreply.github.com>
2023-09-13 18:16:13 -0300

    Update main.js

--- main.js
+++ main.js
@@ -112,7 +112,7 @@ var pinturas = [
 var circlesIdsArray = []; //a array para os ids dos círculos criados
 var qtdCirculosTotal = 0; //quantidade total de círculos utilizados para encobrir a imagem
 var qtdCliquesAteAgora = 0; //quantidade de cliques já realizados para a mostrar a pintura nesta fase
-var qtdCliquesAtePinturaCompleta = 6; //quantidade de cliques até a pintura ser totalmente exibida
+var qtdCliquesAtePinturaCompleta = 5; //quantidade de cliques até a pintura ser totalmente exibida
 var renderizacaoPorContaDeMudancaDeTela = false; //motivo de renderização
 var pinturasRodadasPassadas = []; //array para guardar pinturas que já apareceram em rodadas passadas
 
@@ -213,6 +213,7 @@ function mostrarPintura() {
   //Esta função mostra a pintura pouco a pouco escondendo os círculos brancos
   qtdCliquesAteAgora++;
   console.log(qtdCliquesAteAgora);
+  console.log(renderizacaoPorContaDeMudancaDeTela);
   if (qtdCliquesAteAgora > qtdCliquesAtePinturaCompleta) {
     return;
   } else if (qtdCliquesAteAgora === qtdCliquesAtePinturaCompleta) {
