b687da93a1b5e94df5211906a6290dbfd1fe21cc
gustavo-ribeiro-santiago <gustavorisa@poli.ufrj.br>
2023-12-08 20:07:29 -0300

    changed styles and refactoring

--- projects.js
+++ projects.js
@@ -1,184 +1,258 @@
 const projects = [
   {
-    title:"Issues Management Application",
-    description:"Application developed in 2021 using Power Apps and Power BI as part of a consulting project to a mining company. The application helped users track governmental issues and stakeholders relevant to the company.",
-    cover: "./assets/issues management.jpg",
-    tags:["Professional Work", "Power Platform", "UI/UX"],
-    websiteLink:"https://gustavo-ribeiro-santiago.github.io/issues-management-application/",
-    gitHubRepository:"https://github.com/gustavo-ribeiro-santiago/issues-management-application"
+    title: 'Issues Management Application',
+    description:
+      'Application developed in 2021 using Power Apps and Power BI as part of a consulting project to a mining company. The application helped users track governmental issues and stakeholders relevant to the company.',
+    cover: './assets/issues management.jpg',
+    tags: ['Professional Work', 'Power Platform', 'UI/UX'],
+    websiteLink:
+      'https://gustavo-ribeiro-santiago.github.io/issues-management-application/',
+    gitHubRepository:
+      'https://github.com/gustavo-ribeiro-santiago/issues-management-application',
   },
   {
-    title:"Rio de Janeiro Real Time Bus Tracker",
-    description:"This web application showcases the use of Mapbox and Leaflet to create an interactive map that displays the real-time locations of buses in Rio de Janeiro. The bus location data is fetched from the cityhall's API to display bus markers on the map. It utilizes the Leaflet Markercluster plugin for efficient marker clustering.",
-    cover: "./assets/real time bus tracker project.png",
-    tags:["APIs", "Maps", "MIT Coding Assignment", "Bootstrap"],
-    websiteLink:"https://gustavo-ribeiro-santiago.github.io/real-time-bus-tracker-rj/",
-    gitHubRepository:"https://github.com/gustavo-ribeiro-santiago/real-time-bus-tracker-rj"
+    title: 'Rio de Janeiro Real Time Bus Tracker',
+    description:
+      "This web application showcases the use of Mapbox and Leaflet to create an interactive map that displays the real-time locations of buses in Rio de Janeiro. The bus location data is fetched from the cityhall's API to display bus markers on the map. It utilizes the Leaflet Markercluster plugin for efficient marker clustering.",
+    cover: './assets/real time bus tracker project.png',
+    tags: ['APIs', 'Maps', 'MIT Coding Assignment', 'Bootstrap'],
+    websiteLink:
+      'https://gustavo-ribeiro-santiago.github.io/real-time-bus-tracker-rj/',
+    gitHubRepository:
+      'https://github.com/gustavo-ribeiro-santiago/real-time-bus-tracker-rj',
   },
   {
-    title:"Rioops Task Manager",
-    description:"The Rioops Task Manager is a comprehensive task management system developed from 2019 to 2020 using Microsoft Access and VBA for seamless integration and efficient task tracking within a large shipping company. This tailored solution was designed to streamline, organize and automate various operational tasks, enhancing productivity and ensuring timely deliveries.",
-    cover: "./assets/rioops task manager.png",
-    tags:["Professional Work", "Access", "VBA", "UI/UX"],
-    websiteLink:"https://gustavo-ribeiro-santiago.github.io/sw-task-management/",
-    gitHubRepository:"https://github.com/gustavo-ribeiro-santiago/sw-task-management"
+    title: 'Rioops Task Manager',
+    description:
+      'The Rioops Task Manager is a comprehensive task management system developed from 2019 to 2020 using Microsoft Access and VBA for seamless integration and efficient task tracking within a large shipping company. This tailored solution was designed to streamline, organize and automate various operational tasks, enhancing productivity and ensuring timely deliveries.',
+    cover: './assets/rioops task manager.png',
+    tags: ['Professional Work', 'Access', 'VBA', 'UI/UX'],
+    websiteLink:
+      'https://gustavo-ribeiro-santiago.github.io/sw-task-management/',
+    gitHubRepository:
+      'https://github.com/gustavo-ribeiro-santiago/sw-task-management',
   },
   {
-    title:"Guess the Painting Game",
-    description:"This project is a fun and interactive game that challenges your knowledge of famous paintings and artists. You will be presented with a partially obscured painting, and your goal is to guess the name of the painting and the artist who created it. The catch is that the more of the painting you reveal, the fewer points you can earn.",
-    cover: "./assets/guess the painting game.png",
-    tags:["Game", "Personal", "Tailwind CSS"],
-    websiteLink:"https://gustavo-ribeiro-santiago.github.io/guess-the-painting-game/",
-    gitHubRepository:"https://github.com/gustavo-ribeiro-santiago/guess-the-painting-game"
+    title: 'Guess the Painting Game',
+    description:
+      'This project is a fun and interactive game that challenges your knowledge of famous paintings and artists. You will be presented with a partially obscured painting, and your goal is to guess the name of the painting and the artist who created it. The catch is that the more of the painting you reveal, the fewer points you can earn.',
+    cover: './assets/guess the painting game.png',
+    tags: ['Game', 'Personal', 'Tailwind CSS'],
+    websiteLink:
+      'https://gustavo-ribeiro-santiago.github.io/guess-the-painting-game/',
+    gitHubRepository:
+      'https://github.com/gustavo-ribeiro-santiago/guess-the-painting-game',
   },
   {
-    title:"Star Wars Game",
-    description:"This project was submitted as a requirement for the Industrial Engineering course at UFRJ. It was presented in fulfillment of the requirements for the Programming 2 class in 2016. The game is developed in Python, incorporates Object-Oriented Programming (OOP) principles, and utilizes the Tkinter library for the graphical user interface.",
-    cover: "./assets/star wars game.png",
-    tags:["Game", "University", "Python", "Object-Oriented Programming (OOP)"],
-    websiteLink:"https://gustavo-ribeiro-santiago.github.io/star-wars-game/",
-    gitHubRepository:"https://github.com/gustavo-ribeiro-santiago/star-wars-game"
+    title: 'Star Wars Game',
+    description:
+      'This project was submitted as a requirement for the Industrial Engineering course at UFRJ. It was presented in fulfillment of the requirements for the Programming 2 class in 2016. The game is developed in Python, incorporates Object-Oriented Programming (OOP) principles, and utilizes the Tkinter library for the graphical user interface.',
+    cover: './assets/star wars game.png',
+    tags: ['Game', 'University', 'Python', 'Object-Oriented Programming (OOP)'],
+    websiteLink: 'https://gustavo-ribeiro-santiago.github.io/star-wars-game/',
+    gitHubRepository:
+      'https://github.com/gustavo-ribeiro-santiago/star-wars-game',
   },
   {
-    title:"Pac-Man Animation",
-    description:"A simple Pac-Man animation where the player can create multiple Pac-Men that move around the screen and bounce off the walls. The Pac-Men change directions and display different images to create a fun and interactive animation.",
-    cover: "./assets/pac-man animation.png",
-    tags:["MIT Coding Assignment"],
-    websiteLink:"https://gustavo-ribeiro-santiago.github.io/pac-man-animation/",
-    gitHubRepository:"https://github.com/gustavo-ribeiro-santiago/pac-man-animation"
+    title: 'Pac-Man Animation',
+    description:
+      'A simple Pac-Man animation where the player can create multiple Pac-Men that move around the screen and bounce off the walls. The Pac-Men change directions and display different images to create a fun and interactive animation.',
+    cover: './assets/pac-man animation.png',
+    tags: ['MIT Coding Assignment'],
+    websiteLink:
+      'https://gustavo-ribeiro-santiago.github.io/pac-man-animation/',
+    gitHubRepository:
+      'https://github.com/gustavo-ribeiro-santiago/pac-man-animation',
   },
   {
-    title:"Interactive Follow-the-Mouse Eyes",
-    description:"Experience a captivating animation where a pair of eyes follows your every move. The eyes react to your mouse cursor, adding an element of interactivity and charm to the visual display.",
-    cover: "./assets/eyes.png",
-    tags:["MIT Coding Assignment"],
-    websiteLink:"https://gustavo-ribeiro-santiago.github.io/eyes-exercise/",
-    gitHubRepository:"https://github.com/gustavo-ribeiro-santiago/eyes-exercise"
+    title: 'Interactive Follow-the-Mouse Eyes',
+    description:
+      'Experience a captivating animation where a pair of eyes follows your every move. The eyes react to your mouse cursor, adding an element of interactivity and charm to the visual display.',
+    cover: './assets/eyes.png',
+    tags: ['MIT Coding Assignment'],
+    websiteLink: 'https://gustavo-ribeiro-santiago.github.io/eyes-exercise/',
+    gitHubRepository:
+      'https://github.com/gustavo-ribeiro-santiago/eyes-exercise',
   },
 ];
-const tagsStyles = {
-  "APIs": "primary",
-  "Maps": "secondary",
-  "MIT Coding Assignment": "success",
-  "Professional Work": "dark", 
-  "Power Platform": "success", 
-  "UI/UX": "danger",
-  "Access": "secondary",
-  "VBA": "primary",
-  "Game": "success",
-  "Personal": "warning",
-  "Bootstrap": "danger",
-  "Tailwind CSS" : "info",
-  "University": "warning",
-  "Python": "primary",
-  "Object-Oriented Programming (OOP)": "dark",
-};
-const tagsFilters = {
-  "APIs":false,
-  "Maps":false,
-  "MIT Coding Assignment":false,
-  "Professional Work": false, 
-  "Power Platform": false, 
-  "UI/UX": false,
-  "Access": false,
-  "VBA": false,
-  "Game": false,
-  "Personal": false,
-  "Bootstrap": false,
-  "Tailwind CSS": false,
-  "University": false,
-  "Python": false,
-  "Object-Oriented Programming (OOP)": false,
-};
+const tags = [
+  {
+    tag: 'APIs',
+    style: 'primary',
+    selected: false,
+  },
+  {
+    tag: 'Maps',
+    style: 'secondary',
+    selected: false,
+  },
+  {
+    tag: 'MIT Coding Assignment',
+    style: 'success',
+    selected: false,
+  },
+  {
+    tag: 'Professional Work',
+    style: 'dark',
+    selected: false,
+  },
+  {
+    tag: 'Power Platform',
+    style: 'success',
+    selected: false,
+  },
+  {
+    tag: 'UI/UX',
+    style: 'danger',
+    selected: false,
+  },
+  {
+    tag: 'Access',
+    style: 'secondary',
+    selected: false,
+  },
+  {
+    tag: 'VBA',
+    style: 'primary',
+    selected: false,
+  },
+  {
+    tag: 'Game',
+    style: 'success',
+    selected: false,
+  },
+  {
+    tag: 'Personal',
+    style: 'warning',
+    selected: false,
+  },
+  {
+    tag: 'Bootstrap',
+    style: 'danger',
+    selected: false,
+  },
+  {
+    tag: 'Tailwind CSS',
+    style: 'info',
+    selected: false,
+  },
+  {
+    tag: 'University',
+    style: 'warning',
+    selected: false,
+  },
+  {
+    tag: 'Python',
+    style: 'primary',
+    selected: false,
+  },
+  {
+    tag: 'Object-Oriented Programming (OOP)',
+    style: 'dark',
+    selected: false,
+  },
+];
 let searchedProjects = [];
 
 const renderTagsFilters = () => {
-  // Function to render tags filters on top of page
-  document.getElementById("tags").innerHTML = "";
-  Object.keys(tagsStyles).forEach(tag => {
-    document.getElementById("tags").innerHTML +=
-    `<button type="button" class="btn btn${tagsFilters[tag] ? "" : "-outline"}-${tagsStyles[tag]} mx-1 mt-1 btn-sm ${tag}">${tag}</button>`
+  // Render tags filters on top of page
+  tagsContainer = document.getElementById('tags');
+  tagsContainer.innerHTML = '';
+  tags.forEach(({ tag, style, selected }) => {
+    tagsContainer.innerHTML += `<button type="button" class="btn btn${
+      selected ? '' : '-outline'
+    }-${style} mx-1 mt-1 btn-sm ${tag}">${tag}</button>`;
   });
   // add click event listener to each tag filter
-  Object.keys(tagsStyles).forEach(tag => {
+  tags.forEach(({ tag, selected }, i) => {
     const elements = document.getElementsByClassName(tag);
     for (let e of elements) {
-      e.addEventListener("click", () => {
-        tagsFilters[tag] = !tagsFilters[tag];
+      e.addEventListener('click', () => {
+        tags[i].selected = !selected;
         renderProjects();
         renderTagsFilters();
       });
-    };
+    }
   });
-}
+};
 
 const renderProjects = () => {
-  // Function to render projects' cards on page
-  let htmlProjects = "";
-  let odd = true;
-  projects.forEach(project => {
-    // apply tag filters, if any
-    let hideProject = false;
-    if (Object.values(tagsFilters).indexOf(true) > -1) {
-      hideProject = true;
-      project.tags.forEach(tag => {
-        if (tagsFilters[tag] === true) hideProject = false;
+  // Render projects' cards on page
+  let htmlProjects = '';
+  let selectedTags = tags
+    .filter(({ selected }) => selected)
+    .map(({ tag }) => tag);
+  projects.forEach(
+    ({
+      title,
+      description,
+      cover,
+      websiteLink,
+      gitHubRepository,
+      tags: projTags,
+    }) => {
+      // apply tag filters, if any is selected
+      if (selectedTags.length) {
+        let projSelTags = projTags.filter((tag) => selectedTags.includes(tag));
+        if (!projSelTags.length) return;
+      }
+      // apply filter from searchbar
+      if (searchedProjects.length && !searchedProjects.includes(title)) return;
+      // create html elements
+      let htmlTags = '';
+      projTags.forEach((projTag) => {
+        ({ selected, style } = tags.find(({ tag }) => (tag === projTag)));
+        htmlTags += `<button type="button" class="btn btn${
+          selected ? '' : '-outline'
+        }-${style} btn-sm mx-1 ${projTag} project-tag">${projTag}</button>`;
       });
+      htmlProjects += `
+        <div class="project-div col py-3">
+          <div class="project-card card mx-auto">
+            <a href='${websiteLink}'>
+              <img width="100%" class="bd-placeholder-img card-img-top" src="${cover}" alt="">
+            </a>
+            <div class="card-body">
+              <h5 class="card-title text-dark">${title}</h5>
+              <p class="card-text">${description} <br/> ${htmlTags}</p>
+              <a href="${websiteLink}" class="btn btn-primary mr-1">View Demo</a>
+              <a href="${gitHubRepository}" class="btn btn-secondary mx-1">GitHub Repository</a>
+            </div>
+          </div>
+        </div>`;
     }
-    // apply filter from searchbar
-    if (searchedProjects.length > 0 && searchedProjects.indexOf(project.title) < 0) hideProject = true;
-    if (hideProject === true) return;
-    // create html elements
-    let htmlTags = "";
-    project.tags.forEach(tag => {
-      htmlTags += `<button type="button" class="btn btn${tagsFilters[tag] ? "" : "-outline"}-${tagsStyles[tag]} btn-sm mx-1 ${tag}" 
-      style="padding-top: 2px; padding-bottom: 2px">${tag}</button>`;
-    });
-    htmlProjects += `
-    <div class="col py-3" ${odd ? ' style="background:rgba(240,240,240,0.5)"' : ''}>
-      <div class="card mx-auto w-50">
-        <a href='${project.websiteLink}'>
-          <img width="100%" class="bd-placeholder-img card-img-top" src="${project.cover}" alt="">
-        </a>
-        <div class="card-body">
-          <h5 class="card-title text-dark">${project.title}</h5>
-          <p class="card-text">${project.description} ${htmlTags}</p>
-          <a href="${project.websiteLink}" class="btn btn-primary mr-1">View Demo</a>
-          <a href="${project.gitHubRepository}" class="btn btn-secondary mx-1">GitHub Repository</a>
-        </div>
-      </div>
-    </div>`
-    odd = !odd;
-  });
-  document.getElementById("projects").innerHTML = htmlProjects;
-}
+  );
+  document.getElementById('projects').innerHTML = htmlProjects;
+};
 
-const initializeSearchbar = () => {
+const searchProject = (searchInput) => {
   // Search input in all projects' properties
-  const searchProject = (searchInput) => {
-    searchedProjects = [];
-    projects.forEach(project => {
-      Object.values(project).forEach(property => {
-        if (property.indexOf(searchInput) > -1) searchedProjects.push(project.title)
-      });
-    });
-    renderProjects();
-    renderTagsFilters();
-    if (searchedProjects.length > 0) return;
-    document.getElementById("noResultsAlertContainer").innerHTML = `<div class="alert alert-danger" role="alert">
+  searchedProjects = projects
+    .filter((project) =>
+      Object.values(project).find((prop) => prop.includes(searchInput))
+    )
+    .map(({ title }) => title);
+  renderProjects();
+  renderTagsFilters();
+  if (searchedProjects.length > 0) return;
+  noResults = document.getElementById('noResultsAlertContainer');
+  noResults.innerHTML = `
+  <div class="alert alert-danger" role="alert">
     Sorry. Your search did not return any results. 
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>`;
-    setTimeout(() => document.getElementById("noResultsAlertContainer").innerHTML = "", 8000)
-  };
+  </div>`;
+  setTimeout(() => (noResults.innerHTML = ''), 8000);
+};
+
+const initializeSearchbar = () => {
   // add submit event listener to searchbar
   const searchbar = document.getElementById('searchbar');
   searchbar.addEventListener('submit', (event) => {
-    event.preventDefault(); // prevent page from refreshing
+    event.preventDefault();
     const searchInput = document.getElementById('searchInput').value;
     searchProject(searchInput);
   });
-}
+};
 
 renderProjects();
 renderTagsFilters();
